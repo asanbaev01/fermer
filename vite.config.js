2abf502cf28acaff1b540592ac7995b02f316190
asanbaevasylbek504-png <asanbaevasylbek504@gmail.com>
2026-07-14 14:50:13 +0600

    lkar

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Vercel'де файлдардын жолу туура ачылышы үчүн base кошулду
-  base: './', 
+  base: '/'
 })
